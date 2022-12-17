ed62f0cd55c75b54cc9b06bfa70f79d3feb8afb4
Edyta Mróz <edyta.mroz@aigorithmics.com>
2022-12-17 23:15:03 +0100

    wip

--- build_databases/build.py
+++ build_databases/build.py
@@ -1,7 +1,7 @@
 from zipfile import ZipFile
 import pandas as pd
 
-from mysql_database.schema import Airline, AirPlane, Marketing_Airline
+from mysql_database.schema import Airline, AirPlane, Marketing_Group_Airline
 
 with ZipFile('Combined_Flights_2022.parquet.zip', 'r') as zObject:
     zObject.extractall(path='Combined_Flights_2022.parquet')
@@ -11,7 +11,22 @@ print(df.columns)
 
 
 def write_marketing_airline(values: tuple, columns: list):
-    Marketing_Airline(**{column.lower(): value for column, value in zip(columns, values)}).save()
+    Marketing_Group_Airline(**{column.lower(): value for column, value in zip(columns, values)}).save()
+
+
+foreign_keys_in_airline = {'DOT_ID_Marketing_Airline': lambda x: Marketing_Group_Airline.get(
+    Marketing_Group_Airline.dot_id_marketing_airline == x
+)}
+
+
+def write_airline(values: tuple, columns: list):
+    # foreign_keys_in_airline
+
+    values = [value if column not in foreign_keys_in_airline.keys() else foreign_keys_in_airline[column](value)
+              for column, value in zip(columns, values)]
+    columns = [column if column not in foreign_keys_in_airline.keys() else column + '_id' for column in columns]
+    kwargs = {column.lower(): value for column, value in zip(columns, values)}
+    Airline(**kwargs).save()
 
 
 if __name__ == "__main__":
@@ -23,3 +38,14 @@ if __name__ == "__main__":
     data = df[columns_list].drop_duplicates()
     for item in zip(*[data[name] for name in columns_list]):
         write_marketing_airline(item, columns_list)
+
+
+    columns_list = ['Airline',
+                    'Operating_Airline',
+                    'OriginStateName',
+                    'DOT_ID_Operating_Airline',
+                    'DOT_ID_Marketing_Airline']
+
+    data = df[columns_list].drop_duplicates()
+    for item in zip(*[data[name] for name in columns_list]):
+        write_airline(item, columns_list)
--- mysql_database/schema.py
+++ mysql_database/schema.py
@@ -1,4 +1,5 @@
 from peewee import *
+import datetime
 
 db = MySQLDatabase('test', host='localhost', port=3307, user='test', password='test')
 
@@ -8,7 +9,7 @@ class BaseModel(Model):
         database = db
 
 
-class Marketing_Airline(BaseModel):
+class Marketing_Group_Airline(BaseModel):
     marketing_airline_network = CharField()
     iata_code_marketing_airline = CharField()
     operated_or_branded_code_share_partners = CharField()
@@ -20,7 +21,8 @@ class Airline(BaseModel):
     operating_airline = CharField()
     origin_state_name = CharField()
     dot_id_operating_airline = IntegerField()
-    dot_id_marketing_airline = ForeignKeyField(Marketing_Airline, to_field='dot_id_marketing_airline')
+    dot_id_marketing_airline_id = ForeignKeyField(Marketing_Group_Airline, backref='market_group')
+    # to_field='dot_id_marketing_airline',
 
 
 class AirPlane(BaseModel):
@@ -30,11 +32,14 @@ class AirPlane(BaseModel):
 
 
 if __name__ == "__main__":
-    # db.connect()
-    # dq = Marketing_Airline.delete()
+    db.connect()
+    # dq = Marketing_Group_Airline.delete()
     # dq.execute()
-    # db.create_tables([Marketing_Airline])
+    # dq = Airline.delete()
+    # dq.execute()
+    db.create_tables([Marketing_Group_Airline])
+    db.create_tables([Airline])
 
-    inst = Marketing_Airline.select()#.where(Plane.mileage >= 10)
+    inst = Marketing_Group_Airline.select()#.where(Plane.mileage >= 10)
     for o in inst:
         print(o.marketing_airline_network)
