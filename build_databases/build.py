109c812c89cbd17b0fb1b136609eb19466225b09
Edyta Mróz <edyta.mroz@aigorithmics.com>
2022-12-18 21:18:31 +0100

    wip

--- build_databases/build.py
+++ build_databases/build.py
@@ -18,15 +18,22 @@ foreign_keys_in_airline = {'DOT_ID_Marketing_Airline': lambda x: Marketing_Group
     Marketing_Group_Airline.dot_id_marketing_airline == x
 )}
 
+foreign_keys_in_airplane = {'DOT_ID_Operating_Airline': lambda x: Airline.get(Airline.dot_id_operating_airline == x)}
 
-def write_airline(values: tuple, columns: list):
-    # foreign_keys_in_airline
 
-    values = [value if column not in foreign_keys_in_airline.keys() else foreign_keys_in_airline[column](value)
+def write_airline(values: tuple, columns: list, foreign_keys: dict):
+    values = [value if column not in foreign_keys.keys() else foreign_keys[column](value)
               for column, value in zip(columns, values)]
-    columns = [column if column not in foreign_keys_in_airline.keys() else column + '_id' for column in columns]
+    # columns = [column if column not in foreign_keys_in_airline.keys() else column + '_id' for column in columns]
     kwargs = {column.lower(): value for column, value in zip(columns, values)}
     Airline(**kwargs).save()
+
+def write_airplane(values: tuple, columns: list, foreign_keys: dict):
+    values = [value if column not in foreign_keys.keys() else foreign_keys[column](value)
+              for column, value in zip(columns, values)]
+    # columns = [column if column not in foreign_keys_in_airline.keys() else column + '_id' for column in columns]
+    kwargs = {column.lower(): value for column, value in zip(columns, values)}
+    AirPlane(**kwargs).save()
 
 
 if __name__ == "__main__":
@@ -48,4 +55,13 @@ if __name__ == "__main__":
 
     data = df[columns_list].drop_duplicates()
     for item in zip(*[data[name] for name in columns_list]):
-        write_airline(item, columns_list)
+        write_airline(item, columns_list, foreign_keys_in_airline)
+
+
+    columns_list = ['Tail_Number',
+                    'DOT_ID_Operating_Airline']
+
+    data = df[columns_list].drop_duplicates()
+    for item in zip(*[data[name] for name in columns_list]):
+        write_airplane(item, columns_list, foreign_keys_in_airplane)
+
--- mysql_database/schema.py
+++ mysql_database/schema.py
@@ -21,25 +21,46 @@ class Airline(BaseModel):
     operating_airline = CharField()
     origin_state_name = CharField()
     dot_id_operating_airline = IntegerField()
-    dot_id_marketing_airline_id = ForeignKeyField(Marketing_Group_Airline, backref='market_group')
-    # to_field='dot_id_marketing_airline',
+    dot_id_marketing_airline = ForeignKeyField(Marketing_Group_Airline,
+                                               backref='market_group',
+                                               lazy_load=False)
+                                               # to_field='dot_id_marketing_airline')
 
 
 class AirPlane(BaseModel):
-    tail_number = CharField()
-    total_air_time = FloatField()
-    dot_id_operating_airline = ForeignKeyField(Airline, to_field='dot_id_operating_airline')
+    tail_number = CharField(null=True)
+    # total_air_time = FloatField()
+    dot_id_operating_airline = ForeignKeyField(Airline,
+                                               backref='airline',
+                                               lazy_load=False)
 
 
 if __name__ == "__main__":
-    db.connect()
-    # dq = Marketing_Group_Airline.delete()
+    # db.connect()
+    # dq = AirPlane.delete()
     # dq.execute()
+    #
     # dq = Airline.delete()
     # dq.execute()
-    db.create_tables([Marketing_Group_Airline])
-    db.create_tables([Airline])
+    #
+    # dq = Marketing_Group_Airline.delete()
+    # dq.execute()
+    #
+    # db.create_tables([Marketing_Group_Airline])
+    # db.create_tables([Airline, AirPlane])
 
-    inst = Marketing_Group_Airline.select()#.where(Plane.mileage >= 10)
-    for o in inst:
-        print(o.marketing_airline_network)
+    # inst = Airline.select(
+    #     Marketing_Group_Airline.iata_code_marketing_airline,
+    #     Airline.airline,
+    #     fn.COUNT(Airline.airline).alias('count')).join(Marketing_Group_Airline).group_by(Marketing_Group_Airline.iata_code_marketing_airline).dicts()
+    # for o in inst:
+    #     print(o)
+    #
+    # print('-----')
+    #
+    # inst = Airline.select(Airline.airline).where(Airline.airline == 'Southwest Airlines Co.')
+    # print(len(inst))
+
+    dq = AirPlane.select(AirPlane.tail_number).dicts()
+    for o in dq:
+        print(o)
